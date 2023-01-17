716738ed67ee023f9dd6cd1c8aafc17d7a417cd9
Keito Yokota <yoko0011@algonquinlive.com>
2023-01-16 21:15:37 -0500

    changed element text

--- WEEK1/main.js
+++ WEEK1/main.js
@@ -1,2 +1,4 @@
     console.log("using the main file.")
-    console.log(document.getElementById("title").innerHTML = "something else")
+    // console.log(document.getElementById("title").innerHTML = "something else")
+    document.getElementById("title").innerHTML = "something else"
+
